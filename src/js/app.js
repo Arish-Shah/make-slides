e367ee9eb82361670f121f1c44823a87cc3b9e7c
Arish-Shah <arish0246@gmail.com>
2019-09-08 01:30:49 +0530

    keeping animation in mind

--- src/js/app.js
+++ src/js/app.js
@@ -2,34 +2,45 @@ import '../css/main.scss';
 
 let root = null;
 let slides = null;
+let totalSlides = 0;
 let currentSlide = 0;
 
 export function init(options) {
   //parameters provided during initialization
   if (options) {
-    if (background) document.body.style.background = background;
+    if (options.bgColor) {
+      document.body.style.background = options.bgColor;
+    }
   }
 
   //getting the elements
   root = document.querySelector('.make-slides');
   slides = document.querySelectorAll('.make-slides section');
+  totalSlides = slides.length;
 
-  renderSlide();
-
-  window.onkeyup = keyUpEvent;
+  //event handlers
+  window.onload = loadHandler;
+  window.onhashchange = hashChangeHandler;
+  window.onkeyup = keyUpHandler;
 }
 
-function renderSlide() {
-  while (root.firstChild) {
-    root.removeChild(root.firstChild);
+function loadHandler() {
+  const hash = window.location.hash;
+  if (!hash) {
+    window.location.hash = '/slide-1';
   }
-  root.appendChild(slides[currentSlide]);
 }
 
-function keyUpEvent(event) {
+function hashChangeHandler(event) {
+  const hash = window.location.hash;
+  const prevSlideNumber = event.oldURL.split('#')[1].slice(7);
+  const currentSlideNumber = hash.slice(8);
+}
+
+function keyUpHandler(event) {
   if (event.keyCode === 37) {
-    console.log('prev slide');
+    console.log('prev');
   } else if (event.keyCode === 39) {
-    console.log('next slide');
+    console.log('next');
   }
 }

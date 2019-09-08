77aa91a9ba45b10b8deae91ce455385b79afdfc7
Arish-Shah <arish0246@gmail.com>
2019-09-09 00:38:15 +0530

    added animationDuration as param

--- src/js/app.js
+++ src/js/app.js
@@ -2,45 +2,60 @@ import '../css/main.scss';
 
 let root = null;
 let slides = null;
-let totalSlides = 0;
 let currentSlide = 0;
 
 export function init(options) {
-  //parameters provided during initialization
-  if (options) {
-    if (options.bgColor) {
-      document.body.style.background = options.bgColor;
-    }
-  }
-
   //getting the elements
   root = document.querySelector('.make-slides');
   slides = document.querySelectorAll('.make-slides section');
-  totalSlides = slides.length;
 
-  //event handlers
-  window.onload = loadHandler;
-  window.onhashchange = hashChangeHandler;
+  //parameters provided during initialization
+  setOptions(options);
+
+  //events
+  window.onload = window.onhashchange = loadAndHashChangeHandler;
   window.onkeyup = keyUpHandler;
 }
 
-function loadHandler() {
+function loadAndHashChangeHandler() {
   const hash = window.location.hash;
   if (!hash) {
-    window.location.hash = '/slide-1';
+    window.location.replace('/#/');
+  } else if (/#\/[0-9]*$/i.exec(hash)) {
+    const slideNumber = hash.slice(2) === '' ? 0 : parseInt(hash.slice(2));
+    if (slideNumber < 0) {
+      window.location.replace('/#/');
+    } else if (hash === '#/0') {
+      window.location.replace('/#/');
+    } else if (slideNumber > slides.length) {
+      window.location.replace('/#/' + (slides.length - 1));
+    } else renderSlide(slideNumber);
   }
-}
-
-function hashChangeHandler(event) {
-  const hash = window.location.hash;
-  const prevSlideNumber = event.oldURL.split('#')[1].slice(7);
-  const currentSlideNumber = hash.slice(8);
 }
 
 function keyUpHandler(event) {
   if (event.keyCode === 37) {
-    console.log('prev');
+    if (currentSlide > 0) {
+      currentSlide--;
+      window.location.replace('/#/' + currentSlide);
+    }
   } else if (event.keyCode === 39) {
-    console.log('next');
+    if (currentSlide < slides.length - 1) {
+      currentSlide++;
+      window.location.replace('/#/' + currentSlide);
+    }
+  }
+}
+
+function setOptions(options) {}
+
+function renderSlide(slideNumber) {
+  clearView();
+  root.appendChild(slides[slideNumber]);
+}
+
+function clearView() {
+  while (root.firstChild) {
+    root.removeChild(root.firstChild);
   }
 }

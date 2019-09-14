a5574e4f01064b9e78a7520d1cb4ec6c7681ecf2
Arish-Shah <arish0246@gmail.com>
2019-09-14 14:20:03 +0530

    finished building

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	mode: 'development',
 	entry: './src/js/app.js',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'build'),
 		filename: 'make-slides.js',
 		library: 'makeSlides',
 		libraryTarget: 'umd'
